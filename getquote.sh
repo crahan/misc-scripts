42ac3088a56e27e860387a82398ed3bb37e7b579
Thomas Bouve <crahan@n00.be>
2014-08-07 23:19:01 +0200

    Updated regex

--- getquote.sh
+++ getquote.sh
@@ -5,7 +5,7 @@ newquote=`curl -s http://www.quotedb.com/quote/quote.php?action=random_quote`
 # did it succeed?
 if [ $? -eq 0 ]; then
     # ok, curl was able to pull in a new quote. Parse and cache it
-    echo $newquote | sed "s/^document.write(\'\(.*\)\<br\>.*\<a.*\>\(.*\)\<\/a.*\;$/\"\1\" - \2/" > ~/.qotd
+    echo $newquote | sed "s/^document.write('\(.*\)<br>.*<a.*>\(.*\)<\/a.*;$/\"\1\" - \2/" > ~/.qotd
 fi
 
 cat ~/.qotd
